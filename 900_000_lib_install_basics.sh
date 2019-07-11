a1b6f94ef3e472eeda3399eb062c58dcaef93358
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-11 17:24:12 +0200

    initial commit

--- 900_000_lib_install_basics.sh
+++ 900_000_lib_install_basics.sh
@@ -79,8 +79,11 @@ function install_and_update_language_packs {
         reboot_needed="True"
     fi
 
-    $(which sudo) update-locale LANG=de_AT.UTF-8 LANGUAGE=de_AT:de
-
+    $(which sudo) locale-gen de_AT
+    $(which sudo) locale-gen de_AT.UTF-8
+    $(which sudo) dpkg-reconfigure locales
+    $(which sudo) update-locale LANG="de_AT.UTF-8" LANGUAGE="de_AT"
+    $(which sudo) dpkg-reconfigure locales
 
     local language_support_list=$(check-language-support -l de)
     local language_support
