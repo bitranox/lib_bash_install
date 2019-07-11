95e4663a16527a046570121726f7850fef141868
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-11 15:25:42 +0200

    initial commit

---
+++ 900_000_lib_install_basics.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+function include_dependencies {
+    source /usr/local/lib_bash/lib_color.sh
+    source /usr/local/lib_bash/lib_retry.sh
+    source /usr/local/lib_bash/lib_helpers.sh
+}
+
+include_dependencies  # we need to do that via a function to have local scope of my_dir
+
+
+function install_dialog {
+    if [[ "$(get_is_package_installed dialog)" == "False" ]]; then
+        retry $(which sudo) apt-get install dialog -y > /dev/null 2>&1
+    fi
+}
+
+function install_git {
+    if [[ "$(get_is_package_installed git)" == "False" ]]; then
+        retry $(which sudo) apt-get install git -y > /dev/null 2>&1
+    fi
+}
+
+function install_net_tools {
+    if [[ "$(get_is_package_installed net-tools)" == "False" ]]; then
+        retry $(which sudo) apt-get install net-tools -y > /dev/null 2>&1
+    fi
+}
+
+function uninstall_whoopsie {
+    if [[ "$(get_is_package_installed whoopsie)" == "True" ]]; then
+        retry $(which sudo) apt-get purge whoopsie -y > /dev/null 2>&1
+    fi
+    if [[ "$(get_is_package_installed libwhoopsie0)" == "True" ]]; then
+        retry $(which sudo) apt-get purge libwhoopsie0 -y > /dev/null 2>&1
+    fi
+    if [[ "$(get_is_package_installed libwhoopsie-preferences0)" == "True" ]]; then
+        retry $(which sudo) apt-get purge libwhoopsie-preferences0 -y > /dev/null 2>&1
+    fi
+}
+
+function uninstall_apport {
+    if [[ "$(get_is_package_installed apport)" == "True" ]]; then
+        retry $(which sudo) apt-get purge apport -y > /dev/null 2>&1
+    fi
+}
+
+
+function install_essentials {
+    # update / upgrade linux and clean / autoremove
+    clr_bold clr_green "Installiere Essentielles am Host, entferne Apport und Whoopsie"
+    install_net_tools
+    install_git
+    install_dialog
+    uninstall_whoopsie
+    uninstall_apport
+}
+
+function install_and_update_language_packs {
+    # install language pack and install language files for applications
+    # returns Error 100 if reboot is needed (in variable $?)
+    banner "Install and Update Language Packs"
+    local reboot_needed="False"
+
+    if [[ "$(get_is_package_installed language-pack-de)" == "False" ]]; then
+        retry $(which sudo) apt-get install language-pack-de -y
+        reboot_needed="True"
+    fi
+    if [[ "$(get_is_package_installed language-pack-de-base)" == "False" ]]; then
+        retry $(which sudo) apt-get install language-pack-de-base -y
+        reboot_needed="True"
+    fi
+    if [[ "$(get_is_package_installed manpages-de)" == "False" ]]; then
+        retry $(which sudo) apt-get install manpages-de -y
+        reboot_needed="True"
+    fi
+    if [[ "$(get_is_package_installed language-pack-gnome-de)" == "False" ]]; then
+        retry $(which sudo) apt-get install language-pack-gnome-de -y
+        reboot_needed="True"
+    fi
+
+    $(which sudo) update-locale LANG=\"de_AT.UTF-8\" LANGUAGE=\"de_AT:de\"
+
+
+    local language_support_list=$(check-language-support -l de)
+    local language_support
+    while IFS=$'\n' read -ra language_support_array; do
+      for language_support in "${language_support_array[@]}"; do
+          if [[ "$(get_is_package_installed ${language_support})" == "False" ]]; then
+            retry $(which sudo) apt-get install ${language_support} -y
+            reboot_needed="True"
+          fi
+      done
+    done <<< "${language_support_list}"
+
+    if [[ ${reboot_needed} == "True" ]]; then
+        return 100
+    else
+        return 0
+    fi
+}
+
+
+## make it possible to call functions without source include
+# Check if the function exists (bash specific)
+if [[ ! -z "$1" ]]
+    then
+        if declare -f "${1}" > /dev/null
+        then
+          # call arguments verbatim
+          "$@"
+        else
+          # Show a helpful error
+          function_name="${1}"
+          library_name="${0}"
+          fail "\"${function_name}\" is not a known function name of \"${library_name}\""
+        fi
+	fi
---
+++ install_or_update_lib_bash_install.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# function include_dependencies {
+#     my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
+#     source "${my_dir}/lib_color.sh"
+#     source "${my_dir}/lib_helpers.sh"
+#
+# }
+#
+# include_dependencies  # we need to do that via a function to have local scope of my_dir
+
+function install_or_update_lib_bash {
+    if [[ -d "/usr/local/lib_bash" ]]; then
+        $(which sudo) /usr/local/lib_bash/install_or_update_lib_bash.sh
+    else
+        $(which sudo) git clone https://github.com/bitranox/lib_bash.git /usr/local/lib_bash > /dev/null 2>&1
+        $(which sudo) chmod -R 0755 /usr/local/lib_bash
+        $(which sudo) chmod -R +x /usr/local/lib_bash/*.sh
+        $(which sudo) chown -R root /usr/local/lib_bash || $(which sudo) chown -R ${USER} /usr/local/lib_bash  || echo "giving up set owner" # there is no user root on travis
+        $(which sudo) chgrp -R root /usr/local/lib_bash || $(which sudo) chgrp -R ${USER} /usr/local/lib_bash  || echo "giving up set group" # there is no user root on travis
+    fi
+}
+
+install_or_update_lib_bash
+
+function include_dependencies {
+    source /usr/local/lib_bash/lib_color.sh
+    source /usr/local/lib_bash/lib_retry.sh
+    source /usr/local/lib_bash/lib_helpers.sh
+}
+
+include_dependencies
+
+
+function set_lib_bash_install_permissions {
+    $(which sudo) chmod -R 0755 /usr/local/lib_bash_install
+    $(which sudo) chmod -R +x /usr/local/lib_bash_install/*.sh
+    $(which sudo) chown -R root /usr/local/lib_bash_install || $(which sudo) chown -R ${USER} /usr/local/lib_bash_install || echo "giving up set owner" # there is no user root on travis
+    $(which sudo) chgrp -R root /usr/local/lib_bash_install || $(which sudo) chgrp -R ${USER} /usr/local/lib_bash_install || echo "giving up set group" # there is no user root on travis
+}
+
+function is_lib_bash_install_installed {
+        if [[ -d "/usr/local/lib_bash_install" ]]; then
+            echo "True"
+        else
+            echo "False"
+        fi
+}
+
+
+function is_lib_bash_install_to_update {
+    local git_remote_hash=$(git --no-pager ls-remote --quiet https://github.com/bitranox/lib_bash_install.git | grep HEAD | awk '{print $1;}' )
+    local git_local_hash=$( $(which sudo) cat /usr/local/lib_bash_install/.git/refs/heads/master)
+    if [[ "${git_remote_hash}" == "${git_local_hash}" ]]; then
+        echo "False"
+    else
+        echo "True"
+    fi
+}
+
+function install_lib_bash_install {
+    clr_green "installing lib_bash_install"
+    $(which sudo) git clone https://github.com/bitranox/lib_bash_install.git /usr/local/lib_bash_install > /dev/null 2>&1
+    set_lib_bash_install_permissions
+}
+
+
+function update_lib_bash_install {
+    if [[ $(is_lib_bash_install_to_update) == "True" ]]; then
+        clr_green "lib_bash_install needs to update"
+        (
+            # create a subshell to preserve current directory
+            cd /usr/local/lib_bash_install
+            $(which sudo) git fetch --all  > /dev/null 2>&1
+            $(which sudo) git reset --hard origin/master  > /dev/null 2>&1
+            set_lib_bash_install_permissions
+        )
+        clr_green "lib_bash_install update complete"
+    else
+        clr_green "lib_bash_install is up to date"
+        exit 0
+    fi
+}
+
+function restart_calling_script {
+    local caller_command=("$@")
+    if [ ${#caller_command[@]} -eq 0 ]; then
+        # no parameters passed
+        exit 0
+    else
+        # parameters passed, running the new Version of the calling script
+        "${caller_command[@]}"
+        # exit this old instance with error code 100
+        exit 100
+    fi
+
+}
+
+if [[ $(is_lib_bash_install_installed) == "True" ]]; then
+    update_lib_bash_install
+    restart_calling_script  "${@}"  # needs caller name and parameters
+else
+    install_lib_bash_install
+fi
