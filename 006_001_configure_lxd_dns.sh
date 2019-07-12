5a2eafe53fb56b0ad9b84c98622dd014265c9140
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-12 15:36:41 +0200

    initial commit

--- 006_001_configure_lxd_dns.sh
+++ 006_001_configure_lxd_dns.sh
@@ -45,7 +45,7 @@ function sub_disable_systemd_resolved {
 function sub_configure_network_manager {
     local logfile=$(get_log_file_name "${0}" "${BASH_SOURCE}" )
     backup_file "/etc/NetworkManager/NetworkManager.conf"
-    local line_to_add="[main]\ndns=none\n"
+    local line_to_add="[main]${IFS}dns=none${IFS}"
     replace_or_add_lines_containing_string_in_file "/etc/NetworkManager/NetworkManager.conf" "[main]" "${line_to_add}" "#"
 }
 
